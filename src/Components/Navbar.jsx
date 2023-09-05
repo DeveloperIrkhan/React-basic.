bd8617d4e00a07f7a6127a8fdd8d59a9d78dc9ac
irfan shah <programmer.irfansha@hotmail.com>
2023-09-05 12:52:18 +0500

    Converting Text to UpperCase

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -2,11 +2,10 @@ import PropTypes from 'prop-types'
 import React, { Component } from 'react'
 
 export default class Navbar extends Component {
-
   render() {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <a className="navbar-brand" href="/">Navbar</a>
+        <a className="navbar-brand" href="/">{this.props.title}</a>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
